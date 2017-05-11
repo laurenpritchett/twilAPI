93bab928fdbad22b8f4e1a1f0646079025a21ddb
Lauren Pritchett <laurenpritchett2@gmail.com>
2017-05-11 00:07:53 +0000

    First version of marketing text app

--- helper.py
+++ helper.py
@@ -1,14 +1,30 @@
 import os
 from twilio.rest import Client
+import random
 
 twilio_sid = os.environ['TWILIO_SID']
 twilio_token = os.environ['TWILIO_TOKEN']
 
 client = Client(twilio_sid, twilio_token)
 
-message = client.messages.create(
-    to="+19259806538",
-    from_="+19252906483",
-    body="Hello from Python!")
+BLUSH_MESSAGES = {1: 'Stop by Blush today for $1 off our classic Dragonfruit flavored frozen yogurt!',
+                  2: 'Can\'t beat the heat? Cookies and Cream yogurt is only $1 today at Blush!',
+                  3: 'What\'s better than ice cream? Blush Frozen Yogurt!',
+                  4: 'We miss you at Blush Frozen Yogurt! Come see us soon at Blush!',
+                  5: '$3 large frozen yogurt with toppings at Blush- today only!',
+                  6: 'Your Blush double points are here! Reply YES to claim.',
+                  7: 'Our new Blush Yogurt Mission location is open today!',
+                  }
 
-print(message.sid)
+
+def send_marketing_message():
+    '''Send a random marketing message to the user.'''
+
+    marketing_message = random.choice(BLUSH_MESSAGES.values())
+
+    message = client.messages.create(
+        to='+19259806538',
+        from_='+19252906483',
+        body=marketing_message)
+
+    print(message.sid)
--- server.py
+++ server.py
@@ -1,21 +1,35 @@
+import os
+
 from jinja2 import StrictUndefined
 
-from flask import (Flask, render_template, redirect, flash,
-                   session, request)
+from flask import (Flask, render_template, redirect, flash)
 
-from flask_debugtoolbar import DebugToolbarExtension
+from helper import send_marketing_message
 
 app = Flask(__name__)
 
-app.secret_key = "ABC"
+app.secret_key = 'ABC'
 
 app.jinja_env.undefined = StrictUndefined
 
+
 @app.route('/')
 def index():
-    """Homepage."""
+    'Homepage.'
 
-    return render_template("homepage.html")
-if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 5000))
+    return render_template('homepage.html')
+
+
+@app.route('/send-text', methods=['POST'])
+def send_text():
+    '''Send a random marketing message to the user.'''
+    send_marketing_message()
+    flash('Marketing message delivered to subscribers!')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.debug = False
+    app.jinja_env.auto_reload = app.debug
+    DEBUG = 'NO_DEBUG' not in os.environ
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(port=PORT, host='0.0.0.0', debug=DEBUG)
